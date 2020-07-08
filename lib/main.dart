e07b31304b20f9ce9bff3fea78585862aa72673f
raghav gupta <raghavguptaeng@users.noreply.github.com>
2020-07-08 23:35:47 +0530

    Following is the application to show a simple Id Made using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        title: Text("Ninja Identity Card"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 240,33,0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+//            CircleAvatar(
+//              backgroundImage: AssetImage(),
+//            ),
+
+            Container(
+              child: Text(
+                "NAME",
+                style: TextStyle(
+                  letterSpacing: 2,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+              width: 1,
+            ),
+            Container(
+              child: Text(
+                "Ayushi Chhabra",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+              width: 1,
+            ),
+            Container(
+              child: Text(
+                "AGE",
+                style: TextStyle(
+                  letterSpacing: 2,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+              width: 1,
+            ),
+            Container(
+              child: Text(
+                "18",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+              width: 1,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.blue,
+                ),
+                Text(
+                  "  achhabra2802@gmail.com",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  color: Colors.blue,
+                ),
+                Text(
+                  "  9654445112",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18
+                  ),
+                )
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
